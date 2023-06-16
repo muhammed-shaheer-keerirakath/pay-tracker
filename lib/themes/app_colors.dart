77ad1411cff0216b1c3b10c717c662aeafda4942
Shaheer <shaheer@deriv.com>
2023-06-16 23:18:51 +0400

    style: :lipstick: updates theming of app

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color primary = Color(0xFF17594A);
+  static const Color primaryDark = Color(0xFFE1AEFF);
+  static const Color error = Color(0xFFCC0000);
+  static const Color onError = Color(0xFFFFFFFF);
+}
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pay_tracker/themes/app_colors.dart';
 
 ThemeData themeData = ThemeData(
   useMaterial3: true,
   textTheme: GoogleFonts.poppinsTextTheme(),
   colorScheme: const ColorScheme(
     brightness: Brightness.light,
-    primary: Colors.green,
-    onPrimary: Colors.white,
-    secondary: Colors.green,
-    onSecondary: Colors.black87,
+    primary: AppColors.primary,
+    onPrimary: AppColors.white,
+    secondary: AppColors.primary,
+    onSecondary: AppColors.white,
     error: Colors.red,
     onError: Colors.white,
     background: Colors.white,
     onBackground: Colors.black87,
     surface: Colors.white,
     onSurface: Colors.black87,
-    outline: Colors.black87,
+    outline: AppColors.primary,
   ),
 );
 
@@ -24,16 +25,16 @@ ThemeData darkThemeData = ThemeData(
   useMaterial3: true,
   colorScheme: const ColorScheme(
     brightness: Brightness.dark,
-    primary: Colors.orange,
-    onPrimary: Colors.white70,
-    secondary: Colors.orange,
-    onSecondary: Colors.black54,
+    primary: AppColors.primaryDark,
+    onPrimary: AppColors.black,
+    secondary: AppColors.primaryDark,
+    onSecondary: AppColors.black,
     error: Colors.red,
     onError: Colors.black54,
     background: Colors.black54,
     onBackground: Colors.white70,
     surface: Colors.black54,
     onSurface: Colors.white70,
-    outline: Colors.white70,
+    outline: AppColors.primaryDark,
   ),
 );
